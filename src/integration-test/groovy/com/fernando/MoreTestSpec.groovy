79cead146bb68f4d46f27297c352b1e5a58e2b1c
Fernando <fernando88to@gmail.com>
2022-07-06 08:52:07 -0300

    more.

--- src/integration-test/groovy/com/fernando/MoreTestSpec.groovy
+++ src/integration-test/groovy/com/fernando/MoreTestSpec.groovy
@@ -2,14 +2,14 @@ package com.fernando
 
 import grails.gorm.transactions.Rollback
 import grails.testing.mixin.integration.Integration
-//import org.springframework.transaction.annotation.Transactional
+
 import spock.lang.Specification
 
 @Integration
 @Rollback
 class MoreTestSpec extends Specification {
 
-    //@Transactional
+
     void "usuario possui acesso ao sistema GISE"() {
         given:
             Long total = User.count
